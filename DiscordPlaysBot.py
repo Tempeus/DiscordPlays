4f5cad1396316f13e251d637cc5f430a290e80f3
Tempeus <36718822+Tempeus@users.noreply.github.com>
2023-09-20 23:46:38 -0400

    adding commands

--- DiscordPlaysBot.py
+++ DiscordPlaysBot.py
@@ -4,6 +4,14 @@ from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 
+import keyboard
+import time
+import pydirectinput
+import pyautogui
+import concurrent.futures
+
+from DiscordPlays_KeyCodes import *
+
 # Define your intents
 intents = discord.Intents.default()
 intents.members = False  # Disable typing events, if needed
@@ -17,14 +25,32 @@ print(TOKEN)
 # Initialize the bot with the intents
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-@bot.command(name='hello', help="Say hiiiiii")
-async def hello(ctx):
-    print("executing hello")
-    await ctx.send("Hello, I am your bot!")
-
 @bot.command(name='shoot', help="Shoots gun")
 async def shoot_gun(ctx):
     print("executing shoot")
+    pydirectinput.mouseDown(button="left")
+    time.sleep(1)
+    pydirectinput.mouseUp(button="left")
     await ctx.send("shooting gun")
+
+@bot.command(name='vc', help="Uses voice chat for 10 seconds")
+async def VC(ctx):
+    HoldAndReleaseKey(V, 10)
+    await ctx.send("Holding V for 10 seconds")
+
+@bot.command(name='crouch', help="Holds Crouch for 5 seconds")
+async def crouch(ctx):
+    HoldAndReleaseKey(LEFT_CONTROL, 5)
+    await ctx.send("Holding Crouch for 5 seconds")
+
+@bot.command(name='jump', help="Make me Jump")
+async def crouch(ctx):
+    HoldAndReleaseKey(SPACE, 5)
+    await ctx.send("JUMP")
+
+@bot.command(name='drop', help="Make me drop my gun")
+async def crouch(ctx):
+    HoldAndReleaseKey(G, 0.7)
+    await ctx.send("Dropping gun")
 
 bot.run(TOKEN)
