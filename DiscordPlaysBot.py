504592987e0127bc1cf39c5fece1b6b5e1a7216d
Tempeus <36718822+Tempeus@users.noreply.github.com>
2023-09-22 10:02:00 -0400

    Added multithreading and better TTS

--- DiscordPlaysBot.py
+++ DiscordPlaysBot.py
@@ -7,6 +7,7 @@ from gtts import gTTS
 import playsound
 import os
 import inspirobot
+import random
 
 
 import keyboard
@@ -38,25 +39,43 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
 
 # Functions
-def text_to_speech(msg):
-        name = f"temp{time.time()}.mp3"
-        tts = gTTS(msg)
-        tts.save(name)
-        time.sleep(1) 
-        playsound.playsound(name)
-        os.remove(name)
+def generate_TTS_mp3(msg):
+    name = f"temp{time.time()}.mp3"
+    tts = gTTS(msg)
+    tts.save(name)
+    return name
+
+
+#We dont need this shit anymore
+'''
+
+async def TTS(ctx, msg):
+    name = generate_TTS_mp3(msg)
+    time.sleep(1)
+
+    if ctx.voice_client:
+        source = discord.FFmpegPCMAudio(name)
+        ctx.voice_client.play(source)
+        await ctx.send(msg)
+    else:
+        await ctx.send('I am not in a voice channel. Use $join to make me join.')
+    
+    #can increase duration using Multi-threading?
+    time.sleep(5)
+    os.remove(name)
+'''
 
 def drop_weapon_every_interval(timer_duration):
     for i in range(10):
         # Sleep for the specified duration
         time.sleep(timer_duration)
-        text_to_speech("oops")
+        generate_TTS_mp3("oops")
         HoldAndReleaseKey(G, 0.7)
 
 def reload_paranoia(timer_duration):
     for i in range(20):
         time.sleep(timer_duration)
-        text_to_speech("Reloading")
+        generate_TTS_mp3("Reloading")
         HoldAndReleaseKey(R, 0.7)
 
 # Initialize the bot with the intents
@@ -71,110 +90,165 @@ async def join(ctx):
         channel = ctx.message.author.voice.channel
     await channel.connect()
 
-# @bot.command(name='play_song', help='To play song')
-# async def play(ctx,url):
-#     try :
-#         server = ctx.message.guild
-#         voice_channel = server.voice_client
-
-#         async with ctx.typing():
-#             voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
-#         await ctx.send('**Now playing:** {}'.format(filename))
-#     except:
-#         await ctx.send("The bot is not connected to a voice channel.")
-
 @bot.command(name='shoot', help="Shoots gun")
 async def shoot_gun(ctx):
-    await ctx.send("shooting gun")
-    print("executing shoot")
-    pydirectinput.mouseDown(button="left")
-    time.sleep(1)
-    pydirectinput.mouseUp(button="left")
+    await ctx.send("Shooting Gun", tts=True)
+    def thread():
+        pydirectinput.mouseDown(button="left")
+        time.sleep(1)
+        pydirectinput.mouseUp(button="left")
+    thread_pool.submit(thread)
+
 
 @bot.command(name='vc', help="Uses voice chat for 10 seconds")
 async def VC(ctx):
-    await ctx.send("Holding V for 10 seconds")
-    HoldAndReleaseKey(V, 10)
+    await ctx.send("Using voice chat for the next 10 seconds", tts=True)
+    def thread():
+        HoldAndReleaseKey(V, 10)
+    
+    thread_pool.submit(thread)
+
 
 @bot.command(name='crouch', help="Holds Crouch for 5 seconds")
 async def crouch(ctx):
-    await ctx.send("Holding Crouch for 5 seconds")
-    HoldAndReleaseKey(LEFT_CONTROL, 5)
- 
+    await ctx.send("Crouching for the next 5 seconds", tts=True)
+    def thread():
+        HoldAndReleaseKey(LEFT_CONTROL, 5)
+    thread_pool.submit(thread)
+
 @bot.command(name='jump', help="Make me Jump")
 async def crouch(ctx):
-    await ctx.send("JUMP")
-    HoldAndReleaseKey(SPACE, 5)
+    await ctx.send("Jump", tts=True)
+    def thread():
+        HoldAndReleaseKey(SPACE, 5)
+    thread_pool.submit(thread)
+
 
 @bot.command(name='drop', help="Make me drop my gun")
 async def drop(ctx):
-    await ctx.send("Dropping gun")
-    HoldAndReleaseKey(G, 0.7)
+    await ctx.send("Dropping gun", tts=True)
+    def thread():
+        HoldAndReleaseKey(G, 0.7)
+    thread_pool.submit(thread)
+
 
 @bot.command(name='ult', help="Make me use my ult")
 async def ult(ctx):
-    await ctx.send("Using Ult")
-    HoldAndReleaseKey(X, 0.7)
-    pydirectinput.mouseDown(button="left")
-    time.sleep(1)
-    pydirectinput.mouseUp(button="left")
+    await ctx.send("Using Ultimate", tts=True)
+    def thread():
+        HoldAndReleaseKey(X, 0.7)
+        pydirectinput.mouseDown(button="left")
+        time.sleep(1)
+        pydirectinput.mouseUp(button="left")
+    
+    thread_pool.submit(thread)
+
 
 @bot.command(name='pistol', help="Switch to my pistol")
 async def pistol(ctx):
-    HoldAndReleaseKey(TWO, 0.7)
+    await ctx.send("Switching to pistol", tts=True)
+    def thread():
+        HoldAndReleaseKey(TWO, 0.7)
+
+    thread_pool.submit(thread)
 
 @bot.command(name='knife', help="Switch to my knife")
 async def pistol(ctx):
-    HoldAndReleaseKey(THREE, 0.7)
+    await ctx.send("Switching to knife", tts=True)
+    def thread():
+        HoldAndReleaseKey(THREE, 0.7)
+
+    thread_pool.submit(thread)
 
 @bot.command(name='reload', help="Make me reload")
 async def pistol(ctx):
-    HoldAndReleaseKey(R, 0.7)
+    await ctx.send("Reloading", tts=True)
+    def thread():
+        HoldAndReleaseKey(R, 0.7)
+    
+    thread_pool.submit(thread)
 
 @bot.command(name="random_ability", help="Use a random ability")
 async def random_ability(ctx):
-      print()
+    await ctx.send("Using random ability", tts=True)
+    
+    def thread():
+        ability_bind = [C, LEFT_ALT, E]
+        random_ability = random.choice(ability_bind)
+        HoldAndReleaseKey(random_ability, 0.7)
+        time.sleep(0.3)
+        pydirectinput.mouseDown(button="left")
+        time.sleep(0.25)
+        pydirectinput.mouseUp(button="left")
+
+    thread_pool.submit(thread)
 
 @bot.command(name="ability1", help="using ability 1")
 async def ability_one(ctx):
-    text_to_speech("Initiating Ability Usage Sequence")
-    HoldAndReleaseKey(C, 0.7)
-    time.sleep(0.3)
-    pydirectinput.mouseDown(button="left")
-    time.sleep(0.25)
-    pydirectinput.mouseUp(button="left")
-      
+    await ctx.send("Using ability", tts=True)
+
+    def thread():
+        HoldAndReleaseKey(C, 0.7)
+        time.sleep(0.3)
+        pydirectinput.mouseDown(button="left")
+        time.sleep(0.25)
+        pydirectinput.mouseUp(button="left")
+    
+    thread_pool.submit(thread)
+
 @bot.command(name="ability2", help="using ability 2")
 async def ability_two(ctx):
-    text_to_speech("Initiating Ability Usage Sequence")
-    HoldAndReleaseKey(LEFT_ALT, 0.7)
-    time.sleep(0.3)
-    pydirectinput.mouseDown(button="left")
-    time.sleep(0.25)
-    pydirectinput.mouseUp(button="left")
+    await ctx.send("Using ability", tts=True)
+    
+    def thread():
+        HoldAndReleaseKey(LEFT_ALT, 0.7)
+        time.sleep(0.3)
+        pydirectinput.mouseDown(button="left")
+        time.sleep(0.25)
+        pydirectinput.mouseUp(button="left")
+    
+    thread_pool.submit(thread)
 
 @bot.command(name="ability3", help="using ability 3")
 async def ability_three(ctx):
-    text_to_speech("Initiating Ability Usage Sequence")
-    HoldAndReleaseKey(E, 0.7)
-    time.sleep(0.3)
-    pydirectinput.mouseDown(button="left")
-    time.sleep(0.25)
-    pydirectinput.mouseUp(button="left")
+    await ctx.send("Using ability", tts=True)
+
+    def thread():
+        HoldAndReleaseKey(E, 0.7)
+        time.sleep(0.3)
+        pydirectinput.mouseDown(button="left")
+        time.sleep(0.25)
+        pydirectinput.mouseUp(button="left")
+    
+    thread_pool.submit(thread)
+
+########################################### DEBUFFS #########################################################
+
+@bot.command(name="america", help="Hold down shoot button for 10 seconds")
+async def america(ctx):
+    await ctx.send("What the fuck is a kilometer", tts=True)
+    def tamerica():
+        for i in range(20):
+            time.sleep(0.5)
+            pydirectinput.mouseDown(button="left")
+            time.sleep(0.5)
+            pydirectinput.mouseUp(button="left")
+
+    thread_pool.submit(tamerica)
 
 @bot.command(name="slippery_hands", help="Drop my weapon every 10 seconds 10 times")
 async def slippery_hands(ctx):
-    text_to_speech("Initiating Slippery Hands Sequence")
-    drop_weapon_every_interval(10)
+    await ctx.send("Initiating Slippery Hands Sequence", tts=True)
+    thread_pool.submit(drop_weapon_every_interval,10)
 
 @bot.command(name="reload_paranoia", help="make me reload a gun every 5 seconds 20 times")
 async def reload_paranoia(ctx):
-    text_to_speech("Initiating reloading paranoia")
-    reload_paranoia(5)
+    await ctx.send("Initiating reloading paranoia")
+    thread_pool.submit(reload_paranoia,5)
 
 @bot.command(name='inspire', help="it will inspire you")
 async def inspire(ctx):
     flow = inspirobot.flow()  # Generate a flow object
-    await ctx.send(flow[0].text)
+    await ctx.send(flow[0].text, tts=True)    
 
 bot.run(TOKEN)
